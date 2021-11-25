f47cec2e257f58108a3cdff208116c59d1284a4e
RaZegame <75721601+RaZeSloth@users.noreply.github.com>
2021-11-25 17:40:12 +0200

    Made some imports more natural

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
-import { Client } from 'discord.js';
-import Discord from 'discord.js';
 import { Bot } from './src/util/client';
-
-const client = new Bot()
-import ch from './src/handlers/command_handler';
-import eh from './src/handlers/event_handler';
-ch(client, Discord);
-eh(client, Discord);
+const client = new Bot();
 import dotenv from 'dotenv';
 dotenv.config();
-client.login(process.env.token).catch(() => {
-	console.error('Invalid token!');
-	return process.exit();
-});
+(async () => {
+	await client.spawn(process.env.token).catch(() => {
+		console.error('Invalid token!');
+		return process.exit();
+	});
+})();
--- src/util/client.ts
+++ src/util/client.ts
@@ -1,18 +1,22 @@
-import { Client, Intents, Collection } from "discord.js";
+import Discord, { Client, Intents, Collection } from "discord.js";
 import { command } from "./interfaces";
 
 class Bot extends Client {
-    commands: Collection<string, command>
-	time_stamps: Collection<string, number>
-    cooldown_amount: number;
-    cooldowns: Map<string, Collection<string, number>>
+    public commands: Collection<string, command>
+    public time_stamps: Collection<string, number>
+    public cooldown_amount: number;
+    public cooldowns: Map<string, Collection<string, number>>
     constructor() {
-        super({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+        super({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
         this.commands = new Collection();
         this.cooldowns = new Map();
     }
-
- 
+    async spawn(token: string): Promise<this> {
+        await (await import('../handlers/command_handler')).default(this, Discord);
+       await (await import('../handlers/event_handler')).default(this, Discord);
+        super.login(token);
+        return this;
+    }
 }
 
 export { Bot };
