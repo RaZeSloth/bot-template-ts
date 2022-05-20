a9e5768388cdc4d029b62b012a6408f77a1d049b
RaZe <razeslothc@gmail.com>
2022-05-20 09:07:19 +0300

    Removed dist folder

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("./src/util/client");
-require("dotenv/config");
-const client = new client_1.Bot('Bot ' + process.env.token);
-(async () => await client.start())();
--- dist/src/events/client/ready.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = async (client) => {
-    console.log(`${(await client.getSelf()).username} is online!`);
-};
